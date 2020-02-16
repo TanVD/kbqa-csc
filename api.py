ab5b9969d696f2eaeb19b00bb22337704b439348
Vladislav Tankov <vdtankov@gmail.com>
2020-02-16 17:19:57 +0300

    Use wikipedia search to beat rate limits of google

--- api.py
+++ api.py
@@ -4,8 +4,8 @@ import requests
 from googlesearch import search
 
 
-def get_wikidata_ids(page):
-    url = f"https://ru.wikipedia.org/w/api.php?action=query&prop=pageprops&titles={page}&format=json"
+def get_wikidata_ids(title):
+    url = f"https://ru.wikipedia.org/w/api.php?action=query&prop=pageprops&titles={title}&format=json"
     res = str(requests.get(url).content)
     obj = re.findall("Q\\d+", res)
 
@@ -16,17 +16,51 @@ def get_wikidata_ids(page):
     return obj[0]
 
 
+def search_wikipedia(query):
+    session = requests.Session()
+
+    url = "https://ru.wikipedia.org/w/api.php"
+
+    params = {
+        "action": "query",
+        "format": "json",
+        "list": "search",
+        "srsearch": query
+    }
+
+    result = session.get(url=url, params=params)
+    data = result.json()
+
+    search = data['query']['search']
+    if len(search) == 0:
+        return {"Not found"}
+
+    title = search[0]['title']
+    id = get_wikidata_ids(title)
+    return {id}
+
+
 def search_google(query, max_records=1):
     cur = 1
 
-    for res in search(query=query, lang="ru", domains=["ru.wikipedia.org"], pause=5.0):
+    ids = set()
+
+    for res in search(query=query,
+                      domains=["ru.wikipedia.org"],
+                      num=1,
+                      stop=2,
+                      pause=2.0,
+                      user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36"):
         cur += 1
         page = res[len("https://ru.wikipedia.org/wiki/"):]
         wikidata_id = get_wikidata_ids(page)
         print(query)
         print(wikidata_id)
+        ids.add(wikidata_id)
         if cur >= max_records:
             break
+
+    return ids
 
 
 def print_search(query, max_records=1):
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 
 from deeppavlov import build_model
 
-from api import search_google, print_search
+from api import search_google, print_search, search_wikipedia
 from ner_model import ner
 
 syntactic_model = build_model("ru_syntagrus_joint_parsing")
@@ -75,14 +75,36 @@ questions = json.loads(open("/home/tanvd/CSC/ONTOLOGY_ITMO/data.json", "r").read
 
 
 def main():
+    total_expected = 0
+
+    total = 0
+    total_correct = 0
+    total_incorrect = 0
     for data in questions:
         question = data["q"]
+        expected_ids = set(data["entities"])
+        ids = set()
         print(question)
         entities = syntactic(ner(question))
         for entity in entities:
             query = " ".join([word for (_, word) in sorted(entity.items())])
-            print_search(query)
+            id = search_wikipedia(query)
+            ids.update(id)
+
+        ids_size = len(ids)
+        correct_size = len(ids.intersection(expected_ids))
+        incorrect_size = ids_size - correct_size
+        total += ids_size
+        total_correct += correct_size
+        total_incorrect += incorrect_size
+
+        print(f'CORRECT: {correct_size}')
+        print(f'INCORRECT: {incorrect_size}')
+
         print()
+
+    print(f"TOTAL CORRECT {total_correct}")
+    print(f"TOTAL INCORRECT {total_incorrect}")
 
 
 if __name__ == "__main__":
